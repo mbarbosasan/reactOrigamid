2970399db0d7edffc855804e698941b4bc782dc3
Moises Santos <moisesbarbosa23@gmail.com>
2023-08-12 18:16:06 -0300

    Hooks: useCallback

--- src/App.tsx
+++ src/App.tsx
@@ -1,23 +1,13 @@
 import React from "react";
 
-
 const App = () => {
-    const [carrinho, setCarrinho] = React.useState(0);
-    const [notificacao, setNotificacao] = React.useState<string | null>(null);
-    const timeoutRef = React.useRef<number | undefined>(undefined);
-    function handleClick() {
-        setCarrinho(carrinho + 1)
-        setNotificacao("Item adicionado ao carrinho")
-        clearTimeout(timeoutRef.current)
-        timeoutRef.current = setTimeout(() => {
-            setNotificacao(null)
-        }, 2000)
-        console.log(timeoutRef.current)
-    }
+    const [contar, setContar] = React.useState(0)
 
+    const handleClick = React.useCallback(() => {
+        setContar((contar) => contar + 1)
+    }, [])
     return <>
-        <p>{notificacao}</p>
-        <button onClick={handleClick}>Adicionar Carrinho {carrinho}</button>
+        <button onClick={handleClick}>{contar}</button>
     </>
 };
 
